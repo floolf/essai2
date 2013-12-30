fde42c1c581b37224be123b4e2d8345f11b8f6b8
floolf <flo.bellamine@gmail.com>
2013-12-30 01:37:33 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Essai2::Application.config.secret_token = '86620c3468e94c63a877a650941511597e6b4550dfc846e853d6cb913389c006c4de739a2ec492c2e363ac17db2a4e12bdb86a6b6a413fbefcca7ae5bc84c09b'
